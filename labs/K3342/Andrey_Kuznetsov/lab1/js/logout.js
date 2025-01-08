ce452921be8eaa5819a14e5f60098d546720836e
kdduha <just.andrew.kd@gmail.com>
2025-01-08 23:19:51 +0500

    remove files from old branch

--- labs/K3342/Andrey_Kuznetsov/lab1/js/logout.js
+++
@@ -1,4 +0,0 @@
-document.getElementsByClassName("logout").addEventListener("click", function () {
-    localStorage.removeItem("isAuthenticated"); 
-    window.location.href = '../html/auth.html'; 
-});
